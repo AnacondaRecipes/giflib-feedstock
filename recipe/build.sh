74a40a64f1aa89fb24b5f368ad6aa941ad62dd7d
Kai Tietz <ktietz@anaconda.com>
2020-10-20 13:22:47 +0200

    Update to version 5.2.1

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-./configure --prefix=${PREFIX} --build=$BUILD --host=$HOST
-make
-if [[ $(uname) == Linux ]]; then
-    make check
+cp $RECIPE_DIR/CMakeLists.txt .
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release ..
+make -j$CPU_COUNT
+if [[ "$target_platform" == linux* ]]; then
+  make -C ../tests UTILS=$PWD
 fi
 make install
